c9881657ffbc92e60aada81632e310013dbc7110
yabbah <elmeholt@gmail.com>
2021-03-09 21:58:46 +0100

    Starting the UI

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+password_image = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=password_image)
+canvas.grid(column=0, row=0)
+
+window.mainloop()
